c9cff33ef02eb3c1bda55b600ff1cb2d2092d1e3
Andrew <andrew.j.zeller@gmail.com>
2017-10-29 21:52:15 -0700

    add scripts for testing translation and producing intro audio

---
+++ intro_audio.py
@@ -0,0 +1,13 @@
+from gtts import gTTS
+
+def main():
+    intro = "Welcome to Vocal Focals. I'm now watching for text."
+    audio_output = gTTS(text=output_str, lang='en', slow=False)
+    audio_output_file = 'vocal_focals_intro.mp3'
+    audio_output.save(audio_output_file)
+    print(intro)
+    os.system("mpg321 " + audio_output_file)
+
+if __name__ == '__main__':
+
+    main()
