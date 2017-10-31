89148e7269a7373aab8d6b2db6b6b43d050dad1b
Andrew <andrew.j.zeller@gmail.com>
2017-10-30 21:08:08 -0700

    add scripts for audio on image capture and to test scheduler.

---
+++ capturing_scene_audio.py
@@ -0,0 +1,8 @@
+from gtts import gTTS
+
+audio = "Scene captured... please wait."
+audio_output = gTTS(text=output_str, lang='en', slow=False)
+audio_output_file = 'vocal_focals_capturing_scene.mp3'
+audio_output.save(audio_output_file)
+print(audio)
+os.system("mpg321 " + audio_output_file)
--- intro_audio.py
+++ intro_audio.py
@@ -1,7 +1,7 @@
 from gtts import gTTS
 
 def main():
-    intro = "Welcome to Vocal Focals. I'm now watching for text."
+    intro = "Welcome to Vocal Focals."
     audio_output = gTTS(text=output_str, lang='en', slow=False)
     audio_output_file = 'vocal_focals_intro.mp3'
     audio_output.save(audio_output_file)
